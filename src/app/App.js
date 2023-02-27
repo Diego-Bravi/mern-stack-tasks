e94019c93c767d7e6d5daa87eef773b563bbd607
diego <diegobravi97@gmail.com>
2023-02-27 16:08:59 -0300

    finally commit

--- src/app/App.js
+++ src/app/App.js
@@ -6,15 +6,104 @@ class App extends Component {
         super();
         this.state = {
             title: '',
-            Description: ''
+            description: '',
+            tasks: [],
+            _id: ''
         }
         this.handleChange = this.handleChange.bind(this);
         this.addTask = this.addTask.bind(this);
     }
 
-    addTask() {
-        console.log(this.state);
+    addTask(e) {
         e.preventDefault();
+        if(this.state._id) {
+          fetch(`/api/tasks/${this.state._id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+              title: this.state.title,
+              description: this.state.description
+            }),
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            }
+          })
+            .then(res => res.json())
+            .then(data => {
+              window.M.toast({html: 'Task Updated'});
+              this.setState({_id: '', title: '', description: ''});
+              this.fetchTasks();
+            });
+        } else {
+          fetch('/api/tasks', {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            }
+          })
+            .then(res => res.json())
+            .then(data => {
+              console.log(data);
+              window.M.toast({html: 'Task Saved'});
+              this.setState({title: '', description: ''});
+              this.fetchTasks();
+            })
+            .catch(err => console.error(err));
+        }
+    
+     }
+
+    componentDidMount() {
+        this.fetchTasks();
+
+    }
+
+    fetchTasks() {
+        fetch('/api/tasks')
+            .then (res => res.json())
+            .then(data => { 
+                console.log(data)
+                this.setState({tasks: data});
+                console.log(this.state.tasks);
+                });
+    }
+
+
+    deleteTask(id) {
+       if(confirm('Are you sure you want to delete it ?')) {
+
+        fetch(`/api/tasks/${id}`, {
+            method:'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            M.toast({html: 'Task Delete'}) 
+            this.fetchTasks();
+
+        });
+       }
+    }
+
+    editTask(id){
+        fetch(`/api/tasks/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            this.setState({
+                title: data.title,
+                description:data.description,
+                _id: data._id
+            })
+        
+        })
+
     }
 
     handleChange(e) {
@@ -45,12 +134,12 @@ class App extends Component {
                                     <form onSubmit={this.addTask}>
                                         <div className='row'>
                                             <div className='input-field col s12'>
-                                                <input name="title" onChange={this.handleChange} type="text" placeholder='Task Title'></input>
+                                                <input name="title" onChange={this.handleChange} type="text" placeholder='Task Title' value={this.state.title}></input>
                                             </div>
                                         </div>
                                         <div className='row'>
                                             <div className='input-field col s12'>
-                                                <textarea name="description" placeholder='Task Description' className='materialize-textarea'></textarea>
+                                                <textarea name="description" onChange={this.handleChange}  placeholder='Task Description' className='materialize-textarea' value={this.state.description}></textarea>
                                             </div>
                                         </div>
                                         <button type='submit' className='btn ligth-blue darken-4'>
@@ -61,6 +150,35 @@ class App extends Component {
                             </div>
                         </div>
                         <div className='col s7'>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Title</th>
+                                        <th>Description</th>
+                                    </tr>
+                                    
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.tasks.map(task => {
+                                            return (
+                                                <tr key={task._id}>
+                                                   <td>{task.title}</td> 
+                                                   <td>{task.description}</td>
+                                                   <td>
+                                                    <button className='btn ligth-blue darken-4' onClick={() => this.editTask(task._id)} >
+                                                        <i className='material-icons'>edit</i>
+                                                    </button>
+                                                    <button className='btn ligth-blue darken-4' onClick={() => this.deleteTask(task._id)} style={{margin: '4px'}}>
+                                                        <i className='material-icons'>delete</i>
+                                                    </button>
+                                                    </td> 
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
 
                         </div>
                     </div>
